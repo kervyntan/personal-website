4e79f9654a6d8ab0ac2ca8a9d10be792a438f82e
Kervyn <kervyntan@gmail.com>
2023-07-20 22:06:07 +0800

    update

--- site/src/router/index.ts
+++ site/src/router/index.ts
@@ -14,6 +14,11 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
+      {
+        path: '/helloworld',
+        name: 'HelloWorld',
+        component: () => import(/* webpackChunkName: "helloworld" */ '@/views/HelloWorld.vue'),
+      }
     ],
   },
 ]
