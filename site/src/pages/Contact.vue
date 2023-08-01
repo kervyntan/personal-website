02d9d4c9e76513d380a7cd4f73e0e7dc5c87654b
Kervyn <kervyntan@gmail.com>
2023-08-01 17:48:41 +0800

    responsive done for contact page

--- site/src/pages/Contact.vue
+++ site/src/pages/Contact.vue
@@ -6,7 +6,7 @@ import DownloadCV from '@/views/DownloadCV.vue';
 </script>
 
 <template>
-    <VContainer>
+    <VContainer class="mb-9">
         <div class="text-center">
             <VImg class="mx-auto" :width="200" src="@/assets/plane.png" />
             <h2 class="text-h2 text-white d-inline-flex">
@@ -14,8 +14,8 @@ import DownloadCV from '@/views/DownloadCV.vue';
             </h2>
         </div>
         <VRow justify-sm="center" justify-md="space-between" class="flex-sm-column flex-md-row" style="gap: 2rem;" no-gutters>
-            <ContactForm class="w-75"/>
-            <DownloadCV style="width: 20%;"/>
+            <ContactForm class="flex-1-1" style="width: 70%;"/>
+            <DownloadCV style="width: 25%;"/>
         </VRow>
     </VContainer>
 </template>
--- site/src/views/DownloadCV.vue
+++ site/src/views/DownloadCV.vue
@@ -7,11 +7,13 @@ const downloadResume = () => {
 </script>
 
 <template>
-    <VCard class="download-card" variant="text" rounded="true">
-        <div>
+    <VCard class="download-card flex-1-1" variant="tonal" rounded>
+        <div class="text-center">
             <a href="src/assets/resumes/Kervyn Tan-Resume.pdf" download>
-                <VIcon @click="downloadResume"> mdi-file-download-outline </VIcon>
+                <VIcon size="75" @click="downloadResume"> mdi-file-download-outline </VIcon>
+                <!-- <VCardText> Download CV </VCardText> -->
             </a>
+            <VCardText class="text-white"> View My CV </VCardText>
         </div>
     </VCard>
 </template>
